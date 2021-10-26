037d2cfbe103b847d90e15e33d8c01e91c03e42a
Christopher Chiasson <90638967+ChristopherChiasson139@users.noreply.github.com>
2021-10-26 20:31:21 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace template
+{
+    //DisplayMap()
+    //DisplayMap(int scale) = same;
+    // map legend:
+    // ^ = mountain
+    // ' = grass
+    // ~ = water
+    // * = trees
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
